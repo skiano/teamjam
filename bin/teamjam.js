f353d64a282b2f92230ef00e7604d239a4ea3e0e
Greg Skiano <gregory.skiano@nytimes.com>
2019-04-03 10:54:18 -0700

    better help

--- bin/teamjam.js
+++ bin/teamjam.js
@@ -14,13 +14,19 @@ const argv = require('minimist')(process.argv.slice(2), {
 const help = () => {
   console.log([
     ``,
-    `EXAMPLE USAGE:`,
-    ``,
-    `$ teamjam --help`,
-    `$ teamjam play --url="http://..." --team="MY TEAM"`,
-    `$ teamjam serve --problems=directory/with/problems`,
-    `$ teamjam dev --problems=directory/with/problems`,
+    `USAGE:`,
+    `  teamjam --help`,
+    `  teamjam play --url http://localhost:4000`,
+    `  teamjam serve --problems directory/with/problems`,
+    `  teamjam dev --problems directory/with/problems`,
     '',
+    `FLAGS:`,
+    `  -h, --help`,
+    `  -p, --problems  path to directory of problems to serve/dev`,
+    `  -u, --url       the url of the app you want to play with`,
+    `  -t, --team      your team name while playing`,
+    `  -r, --root      (optional) path you want to download the problems to play`,
+    ``,
   ].join('\n'))
 }
 
